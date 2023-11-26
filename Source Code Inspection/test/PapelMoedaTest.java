d01c328b034cfe3e21f37c21e820ae5915ce3d20
Enzo <enzopellegrini02@hotmail.com>
2023-11-26 15:33:33 -0300

    feat: teste

---
+++ Source Code Inspection/test/PapelMoedaTest.java
@@ -0,0 +1,14 @@
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import org.junit.Test;
+import br.calebe.ticketmachine.core.PapelMoeda;
+
+public class PapelMoedaTest {
+    
+    @Test
+    public void getValorTest() {
+        PapelMoeda papelMoeda = new PapelMoeda(10, 5);
+        int result = papelMoeda.getValor();
+        assertEquals(10, result);
+    }
+}
